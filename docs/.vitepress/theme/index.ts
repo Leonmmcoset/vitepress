1a734fe8af4b647f828c72186e1dccb0dba6dde3
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-07-14 22:51:35 +0800

    giscuz 2

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
 import './vp-code-group.css'
 import './var.css'
+import { h } from 'vue';
+import DefaultTheme from 'vitepress/theme';
+import Comment from './Comment.vue';
 
-export default DefaultTheme
+export default {
+    ...DefaultTheme,
+    Layout() {
+      return h(DefaultTheme.Layout, null, {
+        'doc-after': () => h(Comment),
+      });
+    },
+  };
