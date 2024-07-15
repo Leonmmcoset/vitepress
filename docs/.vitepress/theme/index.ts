81886eb0c11bad0a48d2dd1737c28e5b457a38c1
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-07-15 13:20:08 +0800

    may fix

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,12 @@ import { useData, useRoute } from 'vitepress';
 import './var.css'
 import './vp-code-group'
 
+import DefaultTheme from 'vitepress/theme';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import './var.css'
+import './vp-code-group'
+
 export default {
   extends: DefaultTheme,
 
@@ -46,3 +52,38 @@ export default {
 //     async>
 // </script>
 }
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'leonmmcoset/vitepress', //仓库
+      repoId: 'R_kgDOMTSP1w', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMTSP184CgvyG', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'top',
+      lang: 'zh-CN',
+      },
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    )
+//     <script src="https://giscus.app/client.js"
+//     data-repo="leonmmcoset/vitepress"
+//     data-repo-id="R_kgDOMTSP1w"
+//     data-category="Announcements"
+//     data-category-id="DIC_kwDOMTSP184CgvyG"
+//     data-mapping="pathname"
+//     data-strict="0"
+//     data-reactions-enabled="1"
+//     data-emit-metadata="0"
+//     data-input-position="top"
+//     data-theme="preferred_color_scheme"
+//     data-lang="zh-CN"
+//     crossorigin="anonymous"
+//     async>
+// </script>
+};
