be165efe4936742392ab5100302a8567eabe0f7f
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-07-15 11:13:11 +0800

    fix

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,9 +3,4 @@ import 'var.css'
 import 'vp-code-group.css'
 import Layout from 'Layout.vue'
 
-export default {
-    extends: DefaultTheme,
-    enhanceApp({ app }) {
-        app.component('Layout', Layout)
-    }
-}
+export default DefaultTheme
