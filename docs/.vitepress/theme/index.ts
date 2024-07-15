84db5d51eeb3f27d19cf2818b6c22153460075ad
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-07-15 11:58:05 +0800

    fix

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
-import Layout from 'vitepress/Layout.vue'
+import Layout from './Layout.vue'
 import './var.css'
 import './vp-code-group.css'
 
 export default {
     ...DefaultTheme
-    Layout,
+    Layout: Layout,
 }
