0b8bd861cb109d2b14c0732c2ba88a223b667012
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-07-15 10:49:27 +0800

    fix lot

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,12 +4,14 @@ import './var.css'
 import { h } from 'vue';
 import DefaultTheme from 'vitepress/theme';
 import Comment from './Comment.vue';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 export default {
     ...DefaultTheme,
     Layout() {
       return h(DefaultTheme.Layout, null, {
-        'doc-after': () => h(Comment),
-      });
-    },
+        'doc-after': () => h(Comment)
+      })
+    }
 }
